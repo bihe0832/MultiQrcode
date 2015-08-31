000c1c28c39a7b2205a371dbabae7b0e2d64787a
子勰 <code@bihe0832.com>
2015-08-31 20:28:20 +0800

    提交代码

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # MultiQrcode
+
+基于JS的浏览器类型监测组件
+
+### 背景：
+
+基友做了个APP，结果发了一排二维码，然后互相吐槽了一下，发现移动应用出现也一年了，虽然很简单，但是好像大家都懒得通过写个JS来判断然后提一个二维码让大家下载应用。然后内容不多，而且没啥难度，但是动嘴不如动手，就专门整理了一下。
+
+### 简介：
+
+提供了几个JS接口，通过这几个JS接口，可以轻松获取到当前打开页面的浏览器类型，然后根据类型跳转到对应的链接。同时也提供了两个demo方便大家参考。
+
+二维码：![二维码入场券](./res/qrcode.png "二维码入场券")
+
+### Demo：
+
+#### Demo1 :
+
+Demo1详细展示了各种设备下的页面跳转(PC和各种移动设备）)。不会换起下载，只会打开各种页面。详细的跳转代码参见目录下jump.html
+
+二维码：![二维码入场券](./res/demo1_qrcode.png "二维码入场券")
+
+#### Demo2:
+
+Demo2提供了常见应用下载的二维码的方式中，提供一个二维码支持各种平台下载。详细的跳转代码参见目录下download.html
+
+二维码：![二维码入场券](./res/demo2_qrcode.png "二维码入场券")
+
+#### Tools:
+
+Tools提供了获取是否是移动设备，获取系统，设备类型的各种函数的调用效果。详细的跳转代码参见根目录下tools.html.
+
+
+二维码：![二维码入场券](./res/tools_qrcode.png "二维码入场券")
+
+### 实现原理：
+     
+利用浏览的navigator中的platform字段和userAgent字段来区分请求来源。
+
+### 提供接口：
+     
+#### ZixieJS.browser.isPc
+
+- 功能：判断平台是不是PC
+- 参数：无
+- 返回值：是否为PC设备
+
+#### ZixieJS.browser.getDevices
+
+- 功能：判断设备类型
+- 参数：无
+- 返回值：ZixieJS.browser.devices
+
+#### ZixieJS.browser.getSuperAPP
+
+- 功能：判断是否为超级app
+- 参数：无
+- 返回值：ZixieJS.browser.app
+
+#### ZixieJS.browser.getOSDetail
+
+- 功能：获取具体的PC 操作系统类型和版本号
+- 参数：无
+- 返回值：ZixieJS.browser.OS
+
+#### ZixieJS.browser.getOS
+
+- 功能：获取PC系统类型
+- 参数：无
+- 返回值：ZixieJS.browser.OS
+
+
+### 代码结构：
+     
+     — Demo1: Demo1相关代码
+     	- jump.html: Demo1核心代码
+     — Demo2: Demo2相关代码
+     	- download.html: Demo2核心代码
+     — index.html:Demo首页
+     — res:页面相关资源
+     	- brower.js :核心代码
+     — tools.html:JS调用的整体事例。
+
---
+++ res/brower.js
@@ -0,0 +1,155 @@
+/* code@bihe832.com */
+var ZixieJS = {};
+ZixieJS.browser={};
+//设备定义
+ZixieJS.browser.devices={
+  ipad:"ipad",
+  iphone:"iphone",
+  midp:"midp",
+  android:"android",
+  winCE:"winCE",
+  winPhone:"winPhone",
+  unknow:"unknow"
+};
+//操作系统定义
+ZixieJS.browser.OS={
+  Mac:"Mac",
+  Unix:"Unix",
+  Linux:"Linux",
+  Win:"Win",
+  Win2000:"Win2000",
+  WinXP:"WinXP",
+  Win2003:"Win2003",
+  WinVista:"WinVista",
+  Win7:"Win7",
+  Win8:"Win8",
+  Win10:"Win10",
+  unknow:"unknow"
+};
+//特殊app定义
+ZixieJS.browser.app={
+  uc7:"ipad",
+  ucweb:"iphone",
+  wechat:"wechat",
+  unknow:"unknow"
+};
+
+/**
+ *功  能：判断平台是不是PC
+ *参  数：无
+ *返回值：是否为PC设备
+ */
+ZixieJS.browser.isPc=function(){
+  var system ={
+  		win : false,
+  		mac : false,
+  		xll : false
+  	};
+  	var p = navigator.platform;
+  	system.win = p.indexOf("Win") == 0;
+  	system.mac = p.indexOf("Mac") == 0;
+  	system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+  	if(!(system.win||system.mac||system.xll)){
+  		return false;
+  	}else{
+      return true;
+    }
+
+}
+
+/**
+ *功  能：判断设备类型
+ *参  数：无
+ *返回值：ZixieJS.browser.devices
+ */
+ZixieJS.browser.getDevices=function(){
+  //判断访问设备(userAgent)
+  var sUserAgent = navigator.userAgent.toLowerCase();
+  if(sUserAgent.match(/ipad/i) == "ipad"){
+    return ZixieJS.browser.devices.ipad;
+  }else if(sUserAgent.match(/iphone os/i) == "iphone os"){
+    return ZixieJS.browser.devices.iphone;
+  }else if(sUserAgent.match(/midp/i) == "midp"){
+    return ZixieJS.browser.devices.midp;
+  }else if(sUserAgent.match(/android/i) == "android"){
+    return ZixieJS.browser.devices.android;
+  }else if(sUserAgent.match(/windows ce/i) == "windows ce"){
+    return ZixieJS.browser.devices.winCE;
+  }else if(sUserAgent.match(/windows mobile/i) == "windows mobile"){
+    return ZixieJS.browser.devices.winPhone;
+  }else{
+    return ZixieJS.browser.devices.unknow;
+  }
+}
+
+/**
+ *功  能：判断是否为超级app
+ *参  数：无
+ *返回值：ZixieJS.browser.app
+ */
+ZixieJS.browser.getSuperAPP=function(){
+  //判断访问设备(userAgent)
+  var sUserAgent = navigator.userAgent.toLowerCase();
+  if(sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4"){
+    return ZixieJS.browser.app.uc7;
+  }else if(sUserAgent.match(/ucweb/i) == "ucweb"){
+    return ZixieJS.browser.app.ucweb;
+  }else if(sUserAgent.match(/MicroMessenger/i) == 'micromessenger'){
+    return ZixieJS.browser.app.wechat;
+  }else{
+    return ZixieJS.browser.app.unknow;
+  }
+}
+
+/**
+ *功  能：获取具体的PC 操作系统类型和版本号
+ *参  数：无
+ *返回值：ZixieJS.browser.OS
+ */
+ZixieJS.browser.getOSDetail=function(){
+  // 客户端操作系统
+  var sUserAgent = navigator.userAgent;
+  var isMac = (navigator.platform == "Mac68K") || (navigator.platform == "MacPPC") || (navigator.platform == "Macintosh") || (navigator.platform == "MacIntel"); if (isMac)
+  if (isMac) return ZixieJS.browser.OS.Mac;
+  var isUnix = (navigator.platform == "X11") && !isWin && !isMac;
+  if (isUnix) return ZixieJS.browser.OS.Unix;
+  var isLinux = (String(navigator.platform).indexOf("Linux") > -1);
+  if (isLinux) return ZixieJS.browser.OS.Linux;
+  var isWin = (navigator.platform == "Win32") || (navigator.platform == "Windows");
+  if (isWin) {
+    var isWin2K = sUserAgent.indexOf("Windows NT 5.0") > -1 || sUserAgent.indexOf("Windows 2000") > -1;
+    if (isWin2K) return ZixieJS.browser.OS.Win2000;
+    var isWinXP = sUserAgent.indexOf("Windows NT 5.1") > -1 || sUserAgent.indexOf("Windows XP") > -1;
+    if (isWinXP) return ZixieJS.browser.OS.WinXP;
+    var isWin2003 = sUserAgent.indexOf("Windows NT 5.2") > -1 || sUserAgent.indexOf("Windows 2003") > -1;
+    if (isWin2003) return ZixieJS.browser.OS.Win2003;
+    var isWinVista = sUserAgent.indexOf("Windows NT 6.0") > -1 || sUserAgent.indexOf("Windows Vista") > -1;
+    if (isWinVista) return ZixieJS.browser.OS.WinVista;
+    var isWin7 = sUserAgent.indexOf("Windows NT 6.1") > -1 || sUserAgent.indexOf("Windows 7") > -1;
+    if (isWin7) return ZixieJS.browser.OS.Win7;
+    var isWin8 = sUserAgent.indexOf("Windows NT 6.3") > -1 || sUserAgent.indexOf("Windows 7") > -1;
+    if (isWin8) return ZixieJS.browser.OS.Win8;
+    var isWin10 = sUserAgent.indexOf("Windows NT 10.0") > -1 || sUserAgent.indexOf("Windows 10") > -1;
+    if (isWin10) return ZixieJS.browser.OS.Win10;
+
+  }
+}
+
+/**
+ *功  能：获取PC系统类型
+ *参  数：无
+ *返回值：ZixieJS.browser.OS
+ */
+ZixieJS.browser.getOS=function(){
+  // 客户端操作系统
+  var sUserAgent = navigator.userAgent;
+  var isMac = (navigator.platform == "Mac68K") || (navigator.platform == "MacPPC") || (navigator.platform == "Macintosh") || (navigator.platform == "MacIntel"); if (isMac)
+  if (isMac) return ZixieJS.browser.OS.Mac;
+  var isUnix = (navigator.platform == "X11") && !isWin && !isMac;
+  if (isUnix) return ZixieJS.browser.OS.Unix;
+  var isLinux = (String(navigator.platform).indexOf("Linux") > -1);
+  if (isLinux) return ZixieJS.browser.OS.Linux;
+  var isWin = (navigator.platform == "Win32") || (navigator.platform == "Windows");
+  if (isWin) return ZixieJS.browser.OS.Win;
+  return ZixieJS.browser.OS.unknow;
+}
